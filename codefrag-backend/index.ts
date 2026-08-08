44d52b206791ac85796d2ce2a7d3d465853dae0b
Apurba Halder <apurbahalder.rnc@gmail.com>
2026-08-08 22:32:44 +0530

    feat: add dotenv and mongoose for environment configuration and MongoDB connection

--- codefrag-backend/index.ts
+++ codefrag-backend/index.ts
@@ -1,8 +1,14 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 const app = express();
 const PORT = 5000;
+dotenv.config();
 app.use(cors());
+mongoose.connect(process.env.MONGODB_URI as string)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('CodeFrag backend is running');
