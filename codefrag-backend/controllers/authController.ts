1b88189c932587197fc93bed3e7db47e63129427
Apurba Halder <apurbahalder.rnc@gmail.com>
2026-08-11 12:08:42 +0530

    feat: implement login controller with validation and user authentication logic

--- codefrag-backend/controllers/authController.ts
+++ codefrag-backend/controllers/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { hashPassword, comparePassword } from "../utils/hash";
 import { SignupSchema } from "../schemas/authSchemas";
+import { LoginSchema } from "../schemas/authSchemas";
 import User from "../models/User";
 export const signup = async (req: Request, res: Response) => {
   try {
@@ -32,5 +33,40 @@ export const signup = async (req: Request, res: Response) => {
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: "Internal server error" });
+  }
+};
+export const login = async (req: Request, res: Response) => {
+  try {
+    const result = LoginSchema.safeParse(req.body);
+    if (!result.success) {
+      return res.status(400).json({
+        message: "Invalid input",
+        errors: result.error.issues,
+      });
+    }
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const isPassCorrect = await comparePassword(
+      req.body.password,
+      user.passwordHash,
+    );
+    if (!isPassCorrect) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+    const { passwordHash, ...safeUser } = user.toObject();
+    return res
+      .status(200)
+      .json({ message: "Login successful", user: safeUser });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
   }
 };
