2a613fa2f0ce657bcc1489c0b5a70b0821ba1517
Apurba Halder <apurbahalder.rnc@gmail.com>
2026-08-16 11:45:17 +0530

    feat: refactor runJavaSubmission to improve error handling and maintainability

--- codefrag-backend/judge/runJavaSubmission.ts
+++ codefrag-backend/judge/runJavaSubmission.ts
@@ -7,23 +7,30 @@ import { PassThrough } from "node:stream";
 const docker = new Docker();
 async function runJavaSubmission(code: string, input: string): Promise<string> {
   const tempDir = path.join(os.tmpdir(), Date.now().toString());
-  await fs.mkdir(tempDir, { recursive: true });
-  const javaFilePath = path.join(tempDir, "Solution.java");
-  await fs.writeFile(javaFilePath, code);
-  console.log(`Java file created at: ${javaFilePath}`);
-  const stream = new PassThrough();
-  let output = "";
+  try {
+    await fs.mkdir(tempDir, { recursive: true });
+    const javaFilePath = path.join(tempDir, "Solution.java");
+    await fs.writeFile(javaFilePath, code);
+    console.log(`Java file created at: ${javaFilePath}`);
+    const stream = new PassThrough();
+    let output = "";
 
-  stream.on("data", (chunk) => {
-    output += chunk.toString();
-  });
+    stream.on("data", (chunk) => {
+      output += chunk.toString();
+    });
 
-  await docker.run(
-    "eclipse-temurin:17",
-    ["bash", "-c", "javac /code/Solution.java && java -cp /code Solution"],
-    stream,
-    { HostConfig: { Binds: [`${tempDir}:/code`] }, Tty: false },
-  );
+    await docker.run(
+      "eclipse-temurin:17",
+      ["bash", "-c", "javac /code/Solution.java && java -cp /code Solution"],
+      stream,
+      {
+        HostConfig: { Binds: [`${tempDir}:/code`], AutoRemove: true },
+        Tty: false,
+      },
+    );
 
-  return javaFilePath;
+    return output;
+  } finally {
+    await fs.rm(tempDir, { recursive: true, force: true });
+  }
 }
